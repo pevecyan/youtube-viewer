907cf1ac6c4a244d3b97e3bcc05d3c7269de024a
Žan Pevec <pevecyan@outlook.com>
2025-11-14 23:34:01 +0100

    feature: add callback url

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const videoId = urlParams.get('videoId');
 if (videoId) {
     document.getElementById('videoIframe').src = `https://www.youtube.com/embed/${videoId}`;
 }
+let callbackUrl = urlParams.get('callbackUrl');
+if (callbackUrl) {
+    callbackUrl+=`?videoId=${videoId}&status=played`;
+    document.getElementById('debug').innerText = `Callback URL: ${callbackUrl}`;
+    
+    const img = new Image();
+    img.src = callbackUrl;
+}
